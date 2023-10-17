3139ebfc8f39345c0c3b425b897ddb6af661fbac
Kate Varakina <98549302+VarakinaEkaterina@users.noreply.github.com>
2023-10-17 15:01:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-const path = require("path");
-const fs = require("fs");
+import express from "express";
+const app = express();
+const port = "3000";
+app.get("/as", function (req, res) {
+  res.end("Hello Students!!!");
+});
 
-// console.log("Название файла: ", path.basename(__filename));
-// console.log("Название каталога: ", path.dirname(__filename));
-// console.log("Название расширения: ", path.extname(__filename));
-
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("Папка создана");
-// });
-
-const filePath = path.join(__dirname, "tmp", "2.txt");
-console.log(filePath);
-// fs.writeFile(filePath, "Something wrong in your file", function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("Папка создана");
-// });
-
-// fs.appendFile(filePath, "\nSomething wrong in your file", function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("Папка создана");
-// });
-
-fs.readFile(filePath, "UTF-8", (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
+app.listen(port, () => {
+  console.log("listening on port: " + port);
 });
